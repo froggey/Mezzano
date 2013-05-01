c5467e730dda0a8404f30a7801afcac3ed7aa097
Henry Harrington <henry.harrington@gmail.com>
2013-05-01 21:37:14 +0100

    Coerce stuff to various STRING/BIT-VECTOR types.

--- coerce.lisp
+++ coerce.lisp
@@ -10,6 +10,24 @@
                      (unknown-coercion-object condition)
                      (unknown-coercion-type condition)))))
 
+(defun coerce-vector-element-type (type)
+  "Figure out the element type of the vector type TYPE."
+  (cond
+    ((or (subtypep type 'base-string)
+         (subtypep type 'simple-base-string))
+     'base-char)
+    ((or (subtypep type 'string)
+         (subtypep type 'simple-string))
+     'character)
+    ((or (subtypep type 'bit-vector)
+         (subtypep type 'simple-bit-vector))
+     'bit)
+    (t (let ((expanded-type (typeexpand result-type)))
+         (if (and (consp expanded-type)
+                  (member (first expanded-type) '(array simple-array)))
+             (parse-array-type expanded-type)
+             nil)))))
+
 (defun coerce (object result-type)
   (when (or (eql result-type 't)
             (typep object result-type))
@@ -18,10 +36,12 @@
          (map 'list 'identity object))
         ((subtypep result-type 'vector)
          (check-type object sequence)
-         (let ((element-type (parse-array-type (typeexpand result-type))))
-           (make-array (length object)
-                       :element-type element-type
-                       :initial-contents object)))
+         (let ((element-type (coerce-vector-element-type result-type)))
+           (if element-type
+               (make-array (length object)
+                           :element-type element-type
+                           :initial-contents object)
+               (error 'unknown-coercion :object object :type result-type))))
         ((subtypep result-type 'float)
          (float object))
         (t (error 'unknown-coercion :object object :type result-type))))
