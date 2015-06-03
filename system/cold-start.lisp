21f221d5bff499fe3980879ebcaf5a18ce0a37bc
Henry Harrington <henry.harrington@gmail.com>
2015-06-03 19:39:46 +0100

    Remove the repl from INITIALIZE-LISP.
    It was causing problems with the debug video console.

--- system/cold-start.lisp
+++ system/cold-start.lisp
@@ -269,5 +269,4 @@ structures to exist, and for memory to be allocated, but not much beyond that."
   (makunbound '*warm-llf-files*)
   (mezzano.supervisor:snapshot)
   (write-line "Hello, world.")
-  (terpri)
-  (repl))
+  (terpri))
