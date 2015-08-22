3f7cd3408fb1437eef9ede4c4efd4de5e0025890
Henry Harrington <henry.harrington@gmail.com>
2015-08-22 14:16:43 +0100

    Stream class wrapping disk access.

---
+++ tools/disk-stream.lisp
@@ -0,0 +1,65 @@
+(in-package :sys.int)
+
+(defclass disk-stream (sys.gray:fundamental-binary-input-stream
+                       sys.gray:fundamental-binary-output-stream
+                       file-stream)
+  ((%disk :initarg :disk :reader disk-stream-disk)
+   (%fpos :initarg :position))
+  (:default-initargs :position 0))
+
+(defmethod sys.gray:stream-element-type ((stream disk-stream))
+  '(unsigned-byte 8))
+
+(defmethod sys.gray:stream-file-position ((stream disk-stream) &optional (position-spec nil position-specp))
+  (with-slots (%fpos) stream
+    (cond (position-specp
+           (setf %fpos (if (eql position-spec :end)
+                           (stream-file-length stream)
+                           position-spec)))
+          (t %fpos))))
+
+(defmethod sys.gray:stream-file-length ((stream disk-stream))
+  (let ((disk (disk-stream-disk stream)))
+    (* (mezzano.supervisor:disk-n-sectors disk)
+       (mezzano.supervisor:disk-sector-size disk))))
+
+;;; FIXME: Should limit these to the size of the disk.
+(defmethod sys.gray:stream-read-sequence ((stream disk-stream) seq &optional start end)
+  (let* ((disk (disk-stream-disk stream))
+         (sector-size (mezzano.supervisor:disk-sector-size disk))
+         (n-bytes (- (or end (length seq)) start))
+         (buffer (make-array sector-size :element-type '(unsigned-byte 8) :area :wired))
+         (fpos (file-position stream)))
+    (assert (zerop (rem fpos sector-size)))
+    (assert (zerop (rem n-bytes sector-size)))
+    (dotimes (i (truncate n-bytes sector-size))
+      (multiple-value-bind (successp error)
+          (mezzano.supervisor:disk-read disk
+                                        (truncate fpos sector-size)
+                                        1
+                                        buffer)
+        (when (not successp)
+          (error "Disk read error: ~S" error))
+        (setf (subseq seq (+ start (* i sector-size))) buffer)
+        (incf fpos sector-size)
+        (file-position stream fpos)))))
+
+(defmethod sys.gray:stream-write-sequence ((stream disk-stream) seq &optional start end)
+  (let* ((disk (disk-stream-disk stream))
+         (sector-size (mezzano.supervisor:disk-sector-size disk))
+         (n-bytes (- (or end (length seq)) start))
+         (buffer (make-array sector-size :element-type '(unsigned-byte 8) :area :wired))
+         (fpos (file-position stream)))
+    (assert (zerop (rem fpos sector-size)))
+    (assert (zerop (rem (length buffer) sector-size)))
+    (dotimes (i (truncate n-bytes sector-size))
+      (replace buffer seq :start2 (+ start (* i sector-size)))
+      (multiple-value-bind (successp error)
+          (mezzano.supervisor:disk-write disk
+                                         (truncate fpos sector-size)
+                                         1
+                                         buffer)
+        (when (not successp)
+          (error "Disk read error: ~S" error))
+        (incf fpos sector-size)
+        (file-position stream fpos)))))
