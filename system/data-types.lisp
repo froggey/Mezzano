bf4592d3f346b4f28f2f31adb1988740286c4659
Henry Harrington <henry.harrington@gmail.com>
2016-05-28 19:16:57 +0100

    Bump LLF version.
    Changes to the package system are probably incompatible.

--- system/data-types.lisp
+++ system/data-types.lisp
@@ -228,7 +228,7 @@ Internal to the pager, should not be used by other code.")
 (defconstant +block-map-id-shift+ 8)
 (defconstant +block-map-id-size+ 56)
 
-(defparameter *llf-version* 10)
+(defparameter *llf-version* 11)
 
 (defconstant +llf-end-of-load+ #xFF)
 (defconstant +llf-backlink+ #x01)
