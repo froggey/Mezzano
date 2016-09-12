3a6779255fbec70794718ede09fa67bf8e7a30b7
Henry Harrington <henry.harrington@gmail.com>
2016-09-13 00:32:47 +0100

    Insert trap instructions into the assembly trampolines.

--- tools/cold-generator-arm64.lisp
+++ tools/cold-generator-arm64.lisp
@@ -4,13 +4,16 @@
 (in-package :cold-generator.arm64)
 
 (defparameter *undefined-function-thunk*
-  `((:comment todo))
+  `((:comment todo)
+    (mezzano.lap.arm64:hlt 123))
   "Code for the undefined function thunk.")
 
 (defparameter *closure-trampoline*
-  `((:comment todo))
+  `((:comment todo)
+    (mezzano.lap.arm64:hlt 124))
   "Trampoline used for calling a closure or funcallable-instance via an fref.")
 
 (defparameter *funcallable-instance-trampoline*
-  `((:comment todo))
+  `((:comment todo)
+    (mezzano.lap.arm64:hlt 125))
   "Trampoline used for calling a closure or funcallable-instance via a funcallable-instance.")
