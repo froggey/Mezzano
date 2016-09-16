c590039ad5df8901cf782eff367b426f820bb0d0
Henry Harrington <henry.harrington@gmail.com>
2016-09-16 23:31:33 +0100

    More GDB stuff.

--- tools/gdb.scm
+++ tools/gdb.scm
@@ -1,7 +1,93 @@
 (use-modules ((gdb) #:renamer (symbol-prefix-proc 'gdb:)))
 (use-modules (ice-9 format))
+(use-modules (ice-9 rdelim))
 
-(gdb:execute "display/i $pc")
+(define all-symbols '())
+
+(define (file-write-date object)
+  (if (or (not (string? object))
+          (access? object F_OK))
+      (stat:mtime (stat object))
+      #f))
+
+(define (parse-symbol-file-line line)
+  (let* ((index (string-index line #\Space))
+         (address (substring line 0 index))
+         (name (substring line (1+ index))))
+    (list (string->number address 16)
+          name)))
+
+(define (read-symbol-file path)
+  (let ((result '()))
+    (with-input-from-file path
+      (lambda ()
+        (do ((line (read-line) (read-line)))
+            ((eof-object? line))
+          (set! result (cons (parse-symbol-file-line line) result)))))
+    (sort (reverse result)
+          (lambda (x y) (< (car x) (car y))))))
+
+(define (load-symbols path)
+  (format #t "Loading symbols from ~a~%" path)
+  (set! all-symbols (read-symbol-file path)))
+
+(define (ldsym)
+  (let ((f1 (file-write-date "lispos.map"))
+        (f2 (file-write-date "new.map")))
+    (cond ((and f1 f2)
+           (load-symbols (if (> f1 f2)
+                             "lispos.map"
+                             "new.map")))
+          (f1
+           (load-symbols "lispos.map"))
+          (f2
+           (load-symbols "new.map")))))
+
+(define (lookup name)
+  (define (frob list)
+    (cond ((eq? list '())
+           #f)
+          ((string-ci=? name (car (cdr (car list))))
+           (car (car list)))
+          (#t
+           (frob (cdr list)))))
+  (frob all-symbols))
+
+(define (rlookup address)
+  (define (frob list last)
+    (cond ((or (eq? list '())
+               (< address (car (car list))))
+           last)
+          (#t
+           (frob (cdr list) (car list)))))
+  (define sym (frob all-symbols (list 0 "ukn")))
+  (define base (car sym))
+  (define name (car (cdr sym)))
+  (list name (- address base)))
+
+(define (break name)
+  (define addr (lookup name))
+  (when (not addr)
+    (error "Unknown symbol" name))
+  (gdb:execute (format #f "break *0x~x" addr))
+  addr)
+
+(define (trace)
+  (define pc (gdb:value->integer (gdb:parse-and-eval "$pc")))
+  (define loc (rlookup pc))
+  (define asm (gdb:arch-disassemble (gdb:current-arch) pc #:count 1))
+  (format #t "~8,'0x ~A+~D: ~a~%" pc (car loc) (car (cdr loc)) (cdr (assoc 'asm (car asm)))))
+
+(gdb:register-command!
+ (gdb:make-command "ltrace"
+                   #:invoke (lambda (self args from-tty)
+                              (trace))))
+
+;; This doesn't seem to work?
+;; make-command/register-command doesn't work either.
+;;(gdb:execute "define hook-stop
+;;ltrace
+;;end")
 
 (define (read-value-type address type)
   (let* ((ulong (gdb:arch-ulong-type (gdb:current-arch)))
