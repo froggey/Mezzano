44ec6da0d4c3f753009717cb65fbbd3c1ceff7e6
Henry Harrington <henry.harrington@gmail.com>
2017-02-08 15:08:25 +0000

    Fix wrapping initforms in their declared types.

--- compiler/pass1.lisp
+++ compiler/pass1.lisp
@@ -509,8 +509,8 @@
            initform
            `(the ,declared-type ,initform))))
     (special-variable
-     (let ((declared-type `(and (find-type-declaration (name var) declares)
-                                (mezzano.runtime::symbol-type (name var)))))
+     (let ((declared-type `(and ,(find-type-declaration (name var) declares)
+                                ,(mezzano.runtime::symbol-type (name var)))))
        `(the ,declared-type ,initform)))))
 
 (defun pass1-let* (form env)
