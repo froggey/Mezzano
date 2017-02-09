02598cbb7eb17320a6ea1486ddc9076ba0c2aff0
Henry Harrington <henry.harrington@gmail.com>
2017-02-09 19:15:39 +0000

    Implement the STANDARD-CHAR type with MEMBER instead of SATISFIES.
    SUBTYPEP deals with AND and SATISFIES type really badly.

--- system/type.lisp
+++ system/type.lisp
@@ -393,7 +393,16 @@
 )
 
 (deftype standard-char ()
-  '(and character (satisfies standard-char-p)))
+  '(member
+    #\Space #\Newline
+    #\a #\b #\c #\d #\e #\f #\g #\h #\i #\j #\k #\l #\m
+    #\n #\o #\p #\q #\r #\s #\t #\u #\v #\w #\x #\y #\z
+    #\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M
+    #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z
+    #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\0
+    #\! #\$ #\" #\' #\( #\) #\, #\_ #\- #\. #\/ #\: #\;
+    #\? #\+ #\< #\= #\> #\# #\% #\& #\* #\@ #\[ #\\ #\]
+    #\{ #\| #\} #\` #\^ #\~))
 
 (deftype base-char ()
   'character)
