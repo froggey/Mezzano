9a027af5bae6c2d6241d2ee142c30831bdcb8da5
Henry Harrington <henry.harrington@gmail.com>
2017-02-10 22:56:44 +0000

    Bump LLF version.

--- system/data-types.lisp
+++ system/data-types.lisp
@@ -238,7 +238,7 @@ Internal to the pager, should not be used by other code.")
 reserved on the disk, but no specific block has been allocated.")
 (defconstant +block-map-id-not-allocated+ 0)
 
-(defparameter *llf-version* 18)
+(defparameter *llf-version* 19)
 
 (defconstant +llf-arch-x86-64+ 1)
 (defconstant +llf-arch-arm64+ 2)
