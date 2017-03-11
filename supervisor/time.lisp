619b0bc7b25ef0b84d926c47653944fc89b9ae46
Henry Harrington <henry.harrington@gmail.com>
2017-03-11 20:38:16 +0000

    time

--- supervisor/time.lisp
+++ supervisor/time.lisp
@@ -1,6 +1,13 @@
 ;;;; Copyright (c) 2011-2016 Henry Harrington <henry.harrington@gmail.com>
 ;;;; This code is licensed under the MIT license.
 
+(in-package :sys.int)
+
+(defglobal *rtc-is-utc* t "True if the RTC holds UTC, not local time.")
+(defglobal *time-zone* 0 "CL time zone.")
+
+(defglobal *days-before-month*)
+
 (in-package :mezzano.supervisor)
 
 (defconstant internal-time-units-per-second 1000000)
@@ -13,7 +20,21 @@
 (defun initialize-time ()
   (when (not (boundp '*run-time*))
     (setf *heartbeat-wait-queue* (make-wait-queue :name "Heartbeat wait queue"))
-    (setf *run-time* 0))
+    (setf *run-time* 0)
+    (setf sys.int::*rtc-is-utc* t
+          sys.int::*time-zone* 0)
+    (setf sys.int::*days-before-month*
+          #.(let ((reversed-result nil)
+                  (sum 0))
+              (push nil reversed-result)
+              (dolist (days-in-month '(31 28 31 30 31 30 31 31 30 31 30 31))
+                (push sum reversed-result)
+                (incf sum days-in-month))
+              (coerce (nreverse reversed-result) 'simple-vector))))
+  (setf *run-time-at-boot* *run-time*
+        *boot-time* 0))
+
+(defun initialize-time-late ()
   (setf *run-time-at-boot* *run-time*
         *boot-time* (get-universal-time)))
 
@@ -54,3 +75,35 @@
           (return))
         (wait-for-heartbeat))
      nil))
+
+;;; Derived from SBCL 1.0.55's time code.
+(defun leap-years-before (year)
+  (let ((years (- year 1901)))
+    (+ (- (truncate years 4)
+          (truncate years 100))
+       (truncate (+ years 300) 400))))
+
+(defun encode-universal-time (second minute hour date month year &optional time-zone)
+  (check-type second (mod 60))
+  (check-type minute (mod 60))
+  (check-type hour (mod 24))
+  (check-type date (integer 1 31))
+  (check-type month (integer 1 12))
+  (check-type year (integer 1900))
+  (check-type time-zone (or null rational))
+  (unless time-zone (setf time-zone sys.int::*time-zone*))
+  (let* ((days (+ (1- date)
+                  (svref sys.int::*days-before-month* month)
+                  (if (> month 2)
+                      (leap-years-before (1+ year))
+                      (leap-years-before year))
+                  (* (- year 1900) 365)))
+         (hours (+ hour (* days 24)))
+         (time (+ second (* (+ minute (* (+ hours time-zone) 60)) 60))))
+    time))
+
+(defun get-universal-time ()
+  (multiple-value-bind (second minute hour day month year)
+      (mezzano.supervisor:read-rtc-time)
+    (encode-universal-time second minute hour day month year
+                           (if sys.int::*rtc-is-utc* 0 sys.int::*time-zone*))))
--- system/time.lisp
+++ system/time.lisp
@@ -3,50 +3,6 @@
 
 (in-package :sys.int)
 
-(defvar *rtc-is-utc* t "True if the RTC holds UTC, not local time.")
-(defvar *time-zone* 0 "CL time zone.")
-
-;;; Derived from SBCL 1.0.55's time code.
-(defun leap-years-before (year)
-  (let ((years (- year 1901)))
-    (+ (- (truncate years 4)
-          (truncate years 100))
-       (truncate (+ years 300) 400))))
-
-(defvar *days-before-month*
-  #.(let ((reversed-result nil)
-          (sum 0))
-      (push nil reversed-result)
-      (dolist (days-in-month '(31 28 31 30 31 30 31 31 30 31 30 31))
-        (push sum reversed-result)
-        (incf sum days-in-month))
-      (coerce (nreverse reversed-result) 'simple-vector)))
-
-(defun encode-universal-time (second minute hour date month year &optional time-zone)
-  (check-type second (mod 60))
-  (check-type minute (mod 60))
-  (check-type hour (mod 24))
-  (check-type date (integer 1 31))
-  (check-type month (integer 1 12))
-  (check-type year (integer 1900))
-  (check-type time-zone (or null rational))
-  (unless time-zone (setf time-zone *time-zone*))
-  (let* ((days (+ (1- date)
-                  (aref *days-before-month* month)
-                  (if (> month 2)
-                      (leap-years-before (1+ year))
-                      (leap-years-before year))
-                  (* (- year 1900) 365)))
-         (hours (+ hour (* days 24)))
-         (time (+ second (* (+ minute (* (+ hours time-zone) 60)) 60))))
-    time))
-
-(defun get-universal-time ()
-  (multiple-value-bind (second minute hour day month year)
-      (mezzano.supervisor:read-rtc-time)
-    (encode-universal-time second minute hour day month year
-                           (if *rtc-is-utc* 0 *time-zone*))))
-
 (defconstant +seconds-in-week+ (* 60 60 24 7))
 (defconstant +weeks-offset+ 2145)
 (defconstant +seconds-offset+ 432000)
