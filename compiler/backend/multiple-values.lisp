859fda023e845934aee9d13939de3bd89a0bdaeb
Henry Harrington <henry.harrington@gmail.com>
2017-09-03 12:52:25 +0100

    Stop m-v-flow and dynamic-contours from blowing the stack.

--- compiler/backend/multiple-values.lisp
+++ compiler/backend/multiple-values.lisp
@@ -76,68 +76,75 @@
   t)
 
 (defun multiple-value-flow (function architecture)
-  (let ((flow (make-hash-table)))
+  (let ((flow (make-hash-table))
+        (worklist (list (cons (first-instruction function) nil))))
     ;; DFS over the instructions, following multiple values.
-    (labels ((visit (inst mv-active-p)
-               (cond (mv-active-p
-                      ;; Multiple values are active. Only permitted instructions
-                      ;; are consumers and jumps.
-                      (cond ((consumes-multiple-p inst)
-                             (setf (gethash inst flow) :multiple)
-                             ;; M-V-FUNCALL-M consumes and produces multiple values.
-                             (setf mv-active-p (produces-multiple-p inst)))
-                            ((multiple-value-safe-p inst architecture)
-                             (setf (gethash inst flow) :multiple))
-                            (t
-                             (error "Reached invalid instruction ~S with multiple values active." inst))))
+    (loop
+       (when (endp worklist)
+         (return))
+       (let* ((entry (pop worklist))
+              (inst (car entry))
+              (mv-active-p (cdr entry)))
+         (cond (mv-active-p
+                ;; Multiple values are active. Only permitted instructions
+                ;; are consumers and jumps.
+                (cond ((consumes-multiple-p inst)
+                       (setf (gethash inst flow) :multiple)
+                       ;; M-V-FUNCALL-M consumes and produces multiple values.
+                       (setf mv-active-p (produces-multiple-p inst)))
+                      ((multiple-value-safe-p inst architecture)
+                       (setf (gethash inst flow) :multiple))
                       (t
-                       ;; Multiple values are active. Must not see a consumer.
-                       (when (consumes-multiple-p inst)
-                         (error "Reached MV consuming instruction ~S without multiple values active." inst))
-                       (setf (gethash inst flow) :single)
-                       (setf mv-active-p (produces-multiple-p inst))))
-               (dolist (next (successors function inst))
-                 (let ((next-state (gethash next flow)))
-                   (ecase next-state
-                     ((nil)
-                      (visit next mv-active-p))
-                     (:single
-                      (assert (not mv-active-p)))
-                     (:multiple
-                      (assert mv-active-p)))))))
-      (visit (first (backend-function-code function))
-             nil))
+                       (error "Reached invalid instruction ~S with multiple values active." inst))))
+               (t
+                ;; Multiple values are active. Must not see a consumer.
+                (when (consumes-multiple-p inst)
+                  (error "Reached MV consuming instruction ~S without multiple values active." inst))
+                (setf (gethash inst flow) :single)
+                (setf mv-active-p (produces-multiple-p inst))))
+         (dolist (next (successors function inst))
+           (let ((next-state (gethash next flow)))
+             (ecase next-state
+               ((nil)
+                (push (cons next mv-active-p) worklist))
+               (:single
+                (assert (not mv-active-p)))
+               (:multiple
+                (assert mv-active-p)))))))
     flow))
 
 (defun dynamic-contours (function)
-  (let ((contour (make-hash-table)))
-    ;; DFS over the instructions.
-    (labels ((visit (inst stack)
-               (setf (gethash inst contour) stack)
-               (typecase inst
-                 (begin-nlx-instruction
-                  (push inst stack))
-                 (finish-nlx-instruction
-                  (assert (eql (nlx-region inst) (first stack)))
-                  (pop stack))
-                 (save-multiple-instruction
-                  (push inst stack))
-                 (restore-multiple-instruction
-                  (assert (eql (restore-multiple-context inst)
-                               (save-multiple-context (first stack))))
-                  (pop stack))
-                 (forget-multiple-instruction
-                  (assert (eql (forget-multiple-context inst)
-                               (save-multiple-context (first stack))))
-                  (pop stack)))
-               (dolist (next (successors function inst))
-                 (multiple-value-bind (next-stack visitedp)
-                     (gethash next contour)
-                   (cond
-                     (visitedp
-                      (assert (eql stack next-stack)))
-                     (t
-                      (visit next stack)))))))
-      (visit (first (backend-function-code function))
-             '()))
+  (let ((contour (make-hash-table))
+        (worklist (list (cons (first-instruction function) '()))))
+    (loop
+       (when (endp worklist)
+         (return))
+       (let* ((entry (pop worklist))
+              (inst (car entry))
+              (stack (cdr entry)))
+         (setf (gethash inst contour) stack)
+         (typecase inst
+           (begin-nlx-instruction
+            (push inst stack))
+           (finish-nlx-instruction
+            (assert (eql (nlx-region inst) (first stack)))
+            (pop stack))
+           (save-multiple-instruction
+            (push inst stack))
+           (restore-multiple-instruction
+            (assert (eql (restore-multiple-context inst)
+                         (save-multiple-context (first stack))))
+            (pop stack))
+           (forget-multiple-instruction
+            (assert (eql (forget-multiple-context inst)
+                         (save-multiple-context (first stack))))
+            (pop stack)))
+         (dolist (next (successors function inst))
+           (multiple-value-bind (next-stack visitedp)
+               (gethash next contour)
+             (cond
+               (visitedp
+                (assert (eql stack next-stack)))
+               (t
+                (push (cons next stack) worklist)))))))
     contour))
