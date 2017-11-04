2e029852310f90a84eb259edce7180b0abff32fb
Henry Harrington <henry.harrington@gmail.com>
2017-11-04 21:19:28 +0000

    Enable the new compiler by default.

--- compiler/compiler.lisp
+++ compiler/compiler.lisp
@@ -24,7 +24,7 @@ be generated instead.")
 (defvar *optimize-restrictions* '())
 (defvar *optimize-policy* '(safety 3 debug 3 speed 1))
 
-(defvar *use-new-compiler* nil)
+(defvar *use-new-compiler* t)
 
 (defvar *meters* (make-hash-table))
 
