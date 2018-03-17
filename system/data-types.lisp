57c14346dacb02fc4f7ee0b44f0a7785dde83641
Henry Harrington <henry.harrington@gmail.com>
2018-03-17 12:10:32 +0000

    Bump LLF version. GC changes are incompatible.

--- system/data-types.lisp
+++ system/data-types.lisp
@@ -271,7 +271,7 @@ When the page is written to, the corresponding dirty bit in the card table will 
 reserved on the disk, but no specific block has been allocated.")
 (defconstant +block-map-id-not-allocated+ 0)
 
-(defparameter *llf-version* 21)
+(defparameter *llf-version* 22)
 
 (defconstant +llf-arch-x86-64+ 1)
 (defconstant +llf-arch-arm64+ 2)
