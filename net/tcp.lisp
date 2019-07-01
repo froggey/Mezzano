f2f1874d2086ad10f2939074e53cdcb096084c4f
Bruno Cichon <ebrasca@librepanther.com>
2019-07-01 17:34:54 +0200

    TCP: Add ipv4-address-to-string , close for tcp-listener and export functions

--- net/tcp.lisp
+++ net/tcp.lisp
@@ -654,6 +654,10 @@
   (declare (ignore abort))
   (close-tcp-connection (tcp-stream-connection stream)))
 
+(defmethod close ((listener tcp-listener) &key abort)
+  (declare (ignore abort))
+  (close-tcp-listener listener))
+
 (defmethod open-stream-p ((stream tcp-octet-stream))
   (not (tcp-connection-closed-p stream)))
 
