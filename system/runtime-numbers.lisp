23582097476f6989c06868e7783000d699f9b22f
Henry Harrington <henry.harrington@gmail.com>
2019-07-22 15:01:52 +0100

    Fix conversion of negative bignums to floats

--- system/runtime-numbers.lisp
+++ system/runtime-numbers.lisp
@@ -229,7 +229,7 @@
          (length (integer-length bignum))
          (sig (ldb (byte digits (- length digits)) bignum))
          (exp (expt (float 2 float-zero) (- length digits))))
-    (* (float sig float-zero) exp)))
+    (* (float sig float-zero) exp (if negative -1 1))))
 
 (defun mezzano.runtime::%%coerce-bignum-to-single-float (bignum)
   (bignum-to-float bignum 0.0f0 24))
