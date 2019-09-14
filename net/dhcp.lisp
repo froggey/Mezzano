2f3a9c828caa9d51a9abc550b37bcb8fac7735c1
Henry Harrington <henry.harrington@gmail.com>
2019-09-14 22:48:41 +0100

    dhcp: Fix conversion from subnet mask to prefix length

--- net/dhcp.lisp
+++ net/dhcp.lisp
@@ -230,7 +230,7 @@
 (defvar *dhcp-interactions* (make-hash-table))
 
 (defun netmask-to-prefix-length (netmask)
-  (integer-length (logxor #xFFFFFFFF (ub32ref/be netmask 0))))
+  (- 32 (integer-length (logxor #xFFFFFFFF (ub32ref/be netmask 0)))))
 
 (defun configure-interface-1 (interface lease)
   (let ((interaction (gethash interface *dhcp-interactions*))
