50f5bd94927a14b5534e360dae2ec12f85240e87
Bruno Cichon <ebrasca@librepanther.com>
2019-12-19 22:57:57 +0100

    cache: Update to use page cache

--- file/cache.lisp
+++ file/cache.lisp
@@ -2,11 +2,14 @@
 ;;;; This code is licensed under the MIT license.
 
 (defpackage :mezzano.file-system-cache
-  (:use :cl)
+  (:use :cl :mezzano.disk)
   (:export #:file-cache-stream
            #:file-cache-character-stream
+           #:fs-read-block
+           #:fs-get-block
            #:direction
            #:buffer
+           #:dirty-blocks
            #:file-position*
            #:file-length*))
 
@@ -15,9 +18,14 @@
 (defclass file-cache-stream (file-stream)
   ((%direction :initarg :direction :reader direction)
    (%buffer :initarg :buffer :accessor buffer)
+   (%dirty-blocks :initarg :dirty-blocks :accessor dirty-blocks)
    (%position :initarg :position :accessor file-position*)
-   (%length :initarg :length :accessor file-length*))
-  (:default-initargs :position 0 :length 0))
+   (%length :initarg :length :accessor file-length*)
+   (%block-length :initarg :block-length :accessor block-length))
+  (:default-initargs :buffer (make-hash-table :weakness :key-and-value)
+                     :dirty-blocks (make-hash-table)
+                     :position 0
+                     :length 0))
 
 (defclass file-cache-character-stream (mezzano.internals::external-format-mixin
                                        file-cache-stream)
@@ -41,23 +49,41 @@
 (defmethod output-stream-p ((stream file-cache-stream))
   (member (direction stream) '(:output :io)))
 
+(defgeneric fs-read-block (stream block-n)
+  (:documentation "Read block-n from file"))
+
+(defgeneric fs-get-block (stream)
+  (:documentation "Request new block from FS"))
+
 (defmethod mezzano.gray:stream-write-byte ((stream file-cache-stream) byte)
   (assert (member (direction stream) '(:output :io)))
   (when (>= (file-position* stream) (file-length* stream))
-    (setf (file-length* stream) (1+ (file-position* stream)))
-    (let ((array-length (array-dimension (buffer stream) 0)))
-      (when (>= (file-position* stream) array-length)
-        (setf (buffer stream) (adjust-array (buffer stream) (+ array-length 8192) :initial-element 0)))))
-  (setf (aref (buffer stream) (file-position* stream)) byte)
+    (setf (file-length* stream) (1+ (file-position* stream))))
+  (multiple-value-bind (buffer-n offset)
+      (truncate (file-position* stream) (block-length stream))
+    (let ((buffer (or (gethash buffer-n (buffer stream))
+                      (setf (gethash buffer-n (buffer stream))
+                            (or (fs-read-block stream buffer-n)
+                                (fs-get-block stream))))))
+      (setf (aref buffer offset) byte)
+      (or (gethash buffer-n (dirty-blocks stream))
+          (setf (gethash buffer-n (dirty-blocks stream))
+                buffer))))
   (incf (file-position* stream)))
 
 (defmethod mezzano.gray:stream-read-byte ((stream file-cache-stream))
   (assert (member (direction stream) '(:input :io)))
   (prog1 (if (>= (file-position* stream) (file-length* stream))
              :eof
-             (aref (buffer stream) (file-position* stream)))
+             (multiple-value-bind (buffer-n offset)
+                 (truncate (file-position* stream) (block-length stream))
+               (aref (or (gethash buffer-n (buffer stream))
+                         (setf (gethash buffer-n (buffer stream))
+                               (fs-read-block stream buffer-n)))
+                     offset)))
     (incf (file-position* stream))))
 
+#+nil
 (defmethod mezzano.gray:stream-write-sequence ((stream file-cache-stream) sequence &optional (start 0) end)
   (assert (member (direction stream) '(:output :io)))
   (let* ((end (or end (file-length* stream)))
@@ -71,6 +97,7 @@
              :end2 end2)
     (incf (file-position* stream) (- end2 start))))
 
+#+nil
 (defmethod mezzano.gray:stream-read-sequence ((stream file-cache-stream) sequence &optional (start 0) end)
   (assert (member (direction stream) '(:input :io)))
   (let* ((end (or end (length sequence)))
