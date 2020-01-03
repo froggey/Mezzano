c8325eaf0d88cacba0432f6999524fc5f6d8a37b
Bruno Cichon <ebrasca@librepanther.com>
2020-01-03 20:00:30 +0100

    FAT32: Fix unbalanced parentheses

--- file/fat32.lisp
+++ file/fat32.lisp
@@ -169,7 +169,7 @@ Valid media-type ara 'FAT32   ' " fat-type-label)))
                                :drive-n (aref sector 64) ; Operating system specific
                                :boot-code (subseq sector 90 510))))
     (when (/= (sys.int::ub16ref/le sector 22) 0)
-      error "Bad FATsz16 - got ~D should be 0" (sys.int::ub16ref/le sector 22))
+      (error "Bad FATsz16 - got ~D should be 0" (sys.int::ub16ref/le sector 22)))
     (when (check-signature (aref sector 66))
       (setf (fat-%signature fat32) (aref sector 66)
             (fat-%volume-id fat32) (sys.int::ub32ref/le sector 67)
