bb08f85fb11be159fb58b04f9408273bec218617
Bruno Cichon <ebrasca@librepanther.com>
2020-01-03 19:57:18 +0100

    EXT4: Function directory don't show file . and ..

--- file/ext4.lisp
+++ file/ext4.lisp
@@ -820,8 +820,11 @@
         (path (directory-namestring pathname)))
     (do-files (block offset) disk superblock bgdt inode-n t
       (let* ((file (read-linked-directory-entry block offset))
+             (file-name (linked-directory-entry-name file))
              (type (linked-directory-entry-file-type file)))
-        (unless (= +unknown-type+ type)
+        (unless (or (= +unknown-type+ type)
+                    (string= "." file-name)
+                    (string= ".." file-name))
           (push (parse-simple-file-path host
                                         (format nil
                                                 (if (= +directory-type+ type)
