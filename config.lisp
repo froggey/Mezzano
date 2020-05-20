cadaeb328f77d5d77ef7c8e84ff43cbaf839ed53
Henry Harrington <henry.harrington@gmail.com>
2020-05-20 11:16:04 +0100

    Update config.lisp

--- config.lisp
+++ config.lisp
@@ -4,12 +4,12 @@
 (in-package :mezzano.internals)
 
 ;; This is the local IP address of the system running the file-server.
-(defparameter *file-server-host-ip* '(192 168 0 123))
+(defparameter *file-server-host-ip* "192.168.0.123")
 
 ;; The full path to the Mezzano source tree.
-;; This must be a string and it must end with '/'.
-(defparameter *mezzano-source-path* "/Full/path/to/Mezzano/")
+;; This must be a string, it must end with '/', and it must include a host portion.
+(defparameter *mezzano-source-path* "REMOTE:/Full/path/to/Mezzano/")
 
 ;; The full path to the Mezzano home directory, containing the libraries.
-;; This must be a string and it must end with '/'.
-(defparameter *home-directory-path* "/Full/path/to/home/directory/")
+;; This must be a string, it must end with '/', and it must include a host portion.
+(defparameter *home-directory-path* "REMOTE:/Full/path/to/home/directory/")
