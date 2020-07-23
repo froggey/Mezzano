cfde410a5efd5f53da513e6c565d765b8da3d276
Henry Harrington <henry.harrington@gmail.com>
2020-07-23 21:06:34 +0100

    Update load-sources

--- tools/load-sources.lisp
+++ tools/load-sources.lisp
@@ -31,7 +31,8 @@
         (ensure-directories-exist real-path)
         (when verbose
           (format t "Copy ~S -> ~S~%" f real-path))
-        (copy-file f real-path (guess-element-type-from-extension (pathname-type f)))))))
+        (with-simple-restart (skip "Skip copying file ~S" f)
+          (copy-file f real-path (guess-element-type-from-extension (pathname-type f))))))))
 
 (defun mcclim-setup ()
   ;; Fix up some case issues.
@@ -48,8 +49,10 @@
   ;; ESA and PDF systems.
   (asdf:clear-configuration)
   (asdf:load-system :clim-examples)
+  (asdf:load-system :cl-ppcre)
   (asdf:load-system :clim-listener)
-  (asdf:load-system :beirc)
+  ;(asdf:load-system :beirc)
+  (eval (read-from-string "(mcclim-render-internals::register-all-ttf-fonts (clim:find-port))"))
   (let ((icon-path (translate-logical-pathname
                     (symbol-value
                      (read-from-string "clim-listener::*icon-path*")))))
@@ -60,12 +63,12 @@
                #p"LOCAL:>Icons>"))
   (setf (symbol-value (read-from-string "clim-listener::*icon-path*")) #p"LOCAL:>Icons>")
   (clrhash (symbol-value (read-from-string "clim-listener::*icon-cache*")))
-  (setf (symbol-value (read-from-string "beirc::*beirc-user-init-file*"))
-        #p"LOCAL:>beirc.lisp")
+  ;;(setf (symbol-value (read-from-string "beirc::*beirc-user-init-file*"))
+  ;;      #p"LOCAL:>beirc.lisp")
   (setf mezzano.gui.desktop::*icons*
         (append mezzano.gui.desktop::*icons*
-                '(("LOCAL:>Icons>Chat.png" "beirc" "(beirc:beirc :new-process t)")
-                  ("LOCAL:>Icons>Terminal.png" "Listener" "(clim-listener:run-listener :new-process t)")
+                '(;;("LOCAL:>Icons>Chat.png" "beirc" "(beirc:beirc :new-process t)")
+                  ("LOCAL:>Icons>Terminal.png" "McCLIM Listener" "(clim-listener:run-listener :new-process t)")
                   ("LOCAL:>Icons>Editor.png" "McCLIM Demos" "(mezzano.supervisor:make-thread (lambda ()
                                                                                                (let ((*terminal-io* (make-instance 'mezzano.gui.popup-io-stream:popup-io-stream
                                                                                                                                    :title \"McCLIM Demos console\"))
@@ -83,8 +86,16 @@
            (member "notes" (pathname-directory path) :test #'string-equal)
            (member "prof" (pathname-directory path) :test #'string-equal)
            (member "iso-stage" (pathname-directory path) :test #'string-equal)
+           (member "build-arm64" (pathname-directory path) :test #'string-equal)
+           (member "build-x86-64" (pathname-directory path) :test #'string-equal)
            (eql (pathname-version path) :previous)
+           (string-equal (pathname-name path) "atom-rombios")
+           (string-equal (pathname-name path) "atom-rombios2M")
+           (string-equal (pathname-name path) "cbmemc-1")
            (string-equal (pathname-name path) ".git")
+           (string-equal (pathname-name path) "build-x86-64")
+           (string-equal (pathname-name path) "build-arm64")
+           (string-equal (pathname-type path) "log")
            (string-equal (pathname-type path) "llf")
            (string-equal (pathname-type path) "fasl")
            (string-equal (pathname-type path) "image")
@@ -126,6 +137,80 @@
              (user-homedir-pathname)
              #p"LOCAL:>Videos>"))
 
+(defun warm-up-class-constructors ()
+  "Populate make-instance constructors for every class in the system."
+  (loop
+     for class across (get-all-objects (lambda (object) (typep object 'standard-class)))
+     do (ignore-errors
+          (format t "~S~%" class)
+          (mezzano.clos::safe-class-constructor class))))
+
+(defun load-iota ()
+  ;; Doom & Quake
+  ;; It's assumed that prboom.lisp and sdlquake.lisp have already been built
+  (let ((*default-pathname-defaults* (merge-pathnames "../Iota/"))
+        (mezzano.compiler::*max-optimizer-iterations* 200))
+    (let ((asdf:*central-registry* (list* *default-pathname-defaults* asdf:*central-registry*)))
+      (asdf:load-system :iota))
+    (defpackage :prboom
+      (:use :cl :llvm-runtime)
+      (:export #:make-context #:spawn))
+    (cal "prboom.lisp")
+    (let ((*package* (find-package :prboom)))
+      (eval (read-from-string
+"(defun spawn ()
+  (mezzano.supervisor:make-thread
+   (lambda ()
+     (llvm-runtime:main-1
+      (make-context :current-directory #p\"LOCAL:>Games>Doom>\")
+      ;; -nosfx avoids the delay when quitting, SDL sound doesn't work anyway.
+      '(\"prboom\" \"-nosfx\")
+      '(\"HOME=/Games/Doom/\")))
+   :name \"prboom\"
+   :initial-bindings `((*terminal-io* ,(make-broadcast-stream))
+                       (*standard-input* ,(make-synonym-stream '*terminal-io*))
+                       (*standard-output* ,(make-synonym-stream '*terminal-io*))
+                       (*error-output* ,(make-synonym-stream '*terminal-io*))
+                       (*trace-output* ,(make-synonym-stream '*terminal-io*))
+                       (*debug-io* ,(make-instance 'mezzano.gui.popup-io-stream:popup-io-stream
+                                                   :title \"prboom console\"))
+                       (*query-io* ,(make-synonym-stream '*terminal-io*)))))")))
+    (ensure-directories-exist "LOCAL:>Games>Doom>")
+    (copy-file "prboom.wad" "LOCAL:>Games>Doom>prboom.wad" '(unsigned-byte 8))
+    (copy-file "doom1.wad" "LOCAL:>Games>Doom>doom1.wad" '(unsigned-byte 8))
+    (copy-file "caco.png" "LOCAL:>Icons>" '(unsigned-byte 8))
+    (setf mezzano.gui.desktop::*icons*
+          (append mezzano.gui.desktop::*icons*
+                  (list '("LOCAL:>Icons>caco.png" "Doom" "(prboom:spawn)"))))
+    (defpackage :sdlquake
+      (:use :cl :llvm-runtime)
+      (:export #:make-context #:spawn))
+    (cal "sdlquake.lisp")
+    (let ((*package* (find-package :sdlquake)))
+      (eval (read-from-string
+"(defun spawn ()
+  (mezzano.supervisor:make-thread
+   (lambda ()
+     (llvm-runtime:main-1
+      (make-context :current-directory #p\"LOCAL:>Games>Quake>\")
+      '(\"sdlquake\")
+      '(\"HOME=/Games/Quake/\")))
+   :name \"sdlquake\"
+   :initial-bindings `((*terminal-io* ,(make-broadcast-stream))
+                       (*standard-input* ,(make-synonym-stream '*terminal-io*))
+                       (*standard-output* ,(make-synonym-stream '*terminal-io*))
+                       (*error-output* ,(make-synonym-stream '*terminal-io*))
+                       (*trace-output* ,(make-synonym-stream '*terminal-io*))
+                       (*debug-io* ,(make-instance 'mezzano.gui.popup-io-stream:popup-io-stream
+                                                   :title \"sdlquake console\"))
+                       (*query-io* ,(make-synonym-stream '*terminal-io*)))))")))
+    (ensure-directories-exist "LOCAL:>Games>Quake>id1>")
+    (copy-file "id1/pak0.pak" "LOCAL:>Games>Quake>id1>pak0.pak" '(unsigned-byte 8))
+    (copy-file "quake.png" "LOCAL:>Icons>" '(unsigned-byte 8))
+    (setf mezzano.gui.desktop::*icons*
+          (append mezzano.gui.desktop::*icons*
+                  (list '("LOCAL:>Icons>quake.png" "Quake" "(sdlquake:spawn)"))))))
+
 ;; Cutting a release:
 ;; Start from a clean checkout of mbuild
 ;; Set *lisp-implementation-version* appropriately (system/environment.lisp)
@@ -149,17 +234,24 @@
 ;; connect to with slime
 ;; source locations: (ed '+) (ed 'ql:setup) (ed 'asdf:load-system)
 (defun prepare-release ()
-  ;; Doom & Quake
-  (let ((*default-pathname-defaults* (merge-pathnames "../Iota-old/")))
-    (load "load.lisp"))
+  (load-iota)
   (copy-sources)
   (mcclim-setup)
   (load-quicklisp)
+  (cal "SYS;SOURCE;GUI;COLOUR-MATRIX-DEMO.LISP")
+  (setf mezzano.gui.desktop::*icons*
+        (append mezzano.gui.desktop::*icons*
+                '(("LOCAL:>Icons>Mandelbrot.png" "Postprocessing Demo" "(mezzano.gui.fancy-repl:spawn :initial-function 'colour-matrix-demo:demo :title \"Display Postprocessing Demo\" :width 450 :height 90)"))))
+  (asdf:load-system :mezzano-virgl)
+  (setf mezzano.gui.desktop::*icons*
+        (append mezzano.gui.desktop::*icons*
+                '(("LOCAL:>Icons>Mandelbrot.png" "Virgl 3D Demos" "(mezzano.gui.virgl.demo-menu:spawn)"))))
   ;; Convince ASDF that ASDF and UIOP really are loaded & up to date.
   ;; This is just a hack for the demo & shouldn't be done in general.
   (asdf:register-immutable-system :uiop)
   (asdf:register-immutable-system :asdf)
   (asdf:disable-output-translations)
+  (warm-up-class-constructors)
   (setf (symbol-value (read-from-string "bordeaux-threads:*default-special-bindings*"))
         '((*terminal-io* . (make-instance 'mezzano.gui.popup-io-stream:lazy-popup-io-stream))))
   (setf (sys.int::symbol-global-value '*package*) (find-package :cl-user))
@@ -174,6 +266,7 @@
   (setf mezzano.gui.desktop::*icons*
         (append mezzano.gui.desktop::*icons*
                 '(:next-column
+                  ("LOCAL:>Icons>Editor.png" "Supporters!" "(ed \"LOCAL:>Mezzano>SUPPORTERS.md\")")
                   ("LOCAL:>Icons>Editor.png" "Readme" "(ed \"LOCAL:>Mezzano>README.md\")")
                   ("LOCAL:>Icons>Editor.png" "Quickstart Guide" "(ed \"LOCAL:>Mezzano>doc>quickstart.md\")"))))
   #+(or)
