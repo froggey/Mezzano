2d51f1ed9b9ec7bd13e47b6282385c823f301811
Sylvia Harrington <sylvia.harrington28@gmail.com>
2022-09-08 15:53:17 +0100

    arm64: Fix frame pointer saved by %C-O-W-S-W-I
    How did anything work like this?

--- supervisor/arm64/interrupts.lisp
+++ supervisor/arm64/interrupts.lisp
@@ -54,15 +54,17 @@
     (%enable-interrupts)))
 
 (sys.int::define-lap-function %call-on-wired-stack-without-interrupts ((function unused &optional arg1 arg2 arg3))
+  (:gc :no-frame :layout #*)
+  ;; Argument setup for the frame pointer.
+  (mezzano.lap.arm64:orr :x1 :xzr :x29) ; fp
   ;; Build a frame and save the old stack pointer.
   (mezzano.lap.arm64:stp :x29 :x30 (:pre :sp -16))
-  (:gc :no-frame :layout #*0)
+  (:gc :no-frame :layout #*00)
   (mezzano.lap.arm64:add :x29 :sp :xzr)
   (:gc :frame)
   ;; Argument setup.
   (mezzano.lap.arm64:orr :x6 :xzr :x0) ; function
   (mezzano.lap.arm64:add :x0 :sp 0) ; sp
-  (mezzano.lap.arm64:orr :x1 :xzr :x29) ; fp
   ;; Test if interrupts are enabled.
   (mezzano.lap.arm64:mrs :x9 :daif)
   (mezzano.lap.arm64:cbnz :x9 INTERRUPTS-DISABLED)
